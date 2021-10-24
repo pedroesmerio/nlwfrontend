61d516d9b1ed77b802b798d6c7e019e262076969
pedroesmerio <p.h.esmerio@gmail.com>
2021-10-24 19:03:10 -0300

    Create Loginpage

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,46 @@
+// import { VscGithubInverted } from "react-icons/vsc";
+import styles from './styles.module.scss'
+
+import logoImg from '../../assets/logo.svg'
+
+export function MessageList() {
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+      <ul className={styles.messageList}>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/pedroesmerio.png" alt="Pedro Esmerio" />
+            </div>
+            <span>Pedro Esmerio </span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/pedroesmerio.png" alt="Pedro Esmerio" />
+            </div>
+            <span>Pedro Esmerio </span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/pedroesmerio.png" alt="Pedro Esmerio" />
+            </div>
+            <span>Pedro Esmerio </span>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+  )
+}
+
